c2cd6f3cf986eb5404aed762edb0ef61e453a891
valeron-walkman <v.i.galatenko@gmail.com>
2018-01-30 11:04:22 +0200

    my first tz

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function ($) {
+    $('#large').load("html/large_display.html")
+}) (jQuery);
+
+(function ($) {
+    $('#medium').load("html/medium_display.html")
+}) (jQuery);
+
+(function ($) {
+    $('#small').load("html/small_display.html")
+}) (jQuery);
